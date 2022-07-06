17cd0f29d3ea252b39ec7f65a4c68b4fda3c2010
Jayakrishnan <thatcoder.space@gmail.com>
2022-07-06 20:56:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const hellofunctions = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello hellofunctions`}))
+  res.end()
+  
+}
+
+export default hellofunctions
